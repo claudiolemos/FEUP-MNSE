495f44582e0a9bb1e8c5351f6483074d73b439e0
Cláudio Lemos <cflemos1999@me.com>
2020-05-28 22:29:02 +0100

    Sound module almost finished

--- sound.js
+++ sound.js
@@ -10,7 +10,7 @@ let modMaxDepth = 150;
 let modMinDepth = -150;
 
 let carrierFreq;
-let carrierAmp;
+let modulationAmp;
 let modulationFreq;
 
 function setupSound() {
@@ -19,35 +19,31 @@ function setupSound() {
   carrier.freq(carrierBaseFreq); // set frequency
   carrier.start(); // start oscillating
 
-  modulator = new p5.Oscillator('sine'); // 'sine', 'square' or 'triangle'
+  modulator = new p5.Oscillator('sawtooth'); // 'sine', 'square' or 'triangle'
   modulator.start();
-
+  
   modulator.disconnect(); // add the modulator's output to modulate the carrier's frequency
   carrier.freq(modulator);
 
   analyzer = new p5.FFT(); // create an FFT to analyze the audio
-
   carrier.amp(1.0, 0.01); // toggle audio on
 }
 
 function updateSound(pose, video) {
   if(pose){
     carrierFreq = map(pose.leftWrist.y, 0, video.height, modMinFreq, modMaxFreq);
-    carrierAmp = map(pose.rightWrist.y, 0, video.height, modMinDepth, modMaxDepth);
+    modulationAmp = map(pose.rightWrist.y, 0, video.height, modMinDepth, modMaxDepth);
     modulationFreq = map(pose.nose.x, 0, video.width, 220, 12000);
   }
 
-  let modFreq = map(mouseY, height, 0, modMinFreq, modMaxFreq);
-  modulator.freq(modFreq);
-
-  // change the amplitude of the modulator
-  // negative amp reverses the sawtooth waveform, and sounds percussive
-  let modDepth = map(mouseX, 0, width, modMinDepth, modMaxDepth);
-  modulator.amp(modDepth);
+  carrier.freq(carrierFreq);
+  modulator.freq(modulationFreq);
+  modulator.amp(modulationAmp);
 
   waveform = analyzer.waveform(); // analyze the waveform
 
   // draw the shape of the waveform
+  noFill();
   stroke(255);
   strokeWeight(10);
   beginShape();
@@ -59,8 +55,10 @@ function updateSound(pose, video) {
   endShape();
 
   // adds text with waveform info
-  strokeWeight(1);
-  text('Modulator Frequency: ' + modFreq.toFixed(3) + ' Hz', 20, 20);
-  text('Modulator Amplitude (Modulation Depth): ' + modDepth.toFixed(3), 20, 40);
-  text('Carrier Frequency (pre-modulation): ' + carrierBaseFreq + ' Hz', width / 2, 20);
+  if(pose){
+    strokeWeight(1);
+    text('Modulator Frequency: ' + modulationFreq.toFixed(3) + ' Hz', 20, 20);
+    text('Modulator Amplitude (Modulation Depth): ' + modulationAmp.toFixed(3), 20, 40);
+    text('Carrier Frequency (pre-modulation): ' + carrierFreq + ' Hz', width / 2, 20);
+  }
 }
